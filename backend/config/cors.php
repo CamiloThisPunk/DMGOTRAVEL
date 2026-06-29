533f8e8047f9c619e0ef9c33ded8d06c2ca92628
Camilo <camiloharol80@gmail.com>
2026-06-29 13:27:29 -0500

    Update CORS to allow port 5175

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://127.0.0.1:5176', 'http://localhost:5176', 'http://127.0.0.1:5174', 'http://localhost:5174'],
+    'allowed_origins' => ['http://127.0.0.1:5176', 'http://localhost:5176', 'http://127.0.0.1:5174', 'http://localhost:5174', 'http://127.0.0.1:5175', 'http://localhost:5175'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
